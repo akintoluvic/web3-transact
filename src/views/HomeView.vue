3c28bc996f828af34ae8445f0daedf3b5319b63b
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-28 14:46:42 +0100

    issue reloading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script>
+import { onMounted } from "vue";
+import Web3 from "web3";
 
+export default {
+  setup() {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+      }
+    };
+
+    const load = async () => {
+      await loadWeb3();
+      updateStatus("Ready!");
+    };
+
+    const updateStatus = async (status) => {
+      // const statusEl = document.getElementById('status');
+      // statusEl.innerHTML = status;
+      console.log(status);
+    };
+
+    onMounted(() => {
+      load();
+    });
+  },
+};
+</script>
 <template>
   <main>
     <h1>This is an Home Page</h1>
